52f86648c3822eea2b1f3428e0990fda198a4716
Vivek <bombatkar.vivek@gmail.com>
2018-07-10 17:53:38 +0200

    initial checkin

---
+++ src/main/detect_text_from_image.py
@@ -0,0 +1,107 @@
+"""
+Example Usage:
+python detect.py text ./resources/wakeupcat.jpg
+python detect.py labels ./resources/landmark.jpg
+python detect.py web ./resources/landmark.jpg
+python detect.py web-uri http://wheresgus.com/dog.JPG
+python detect.py web-geo ./resources/city.jpg
+python detect.py faces-uri gs://your-bucket/file.jpg
+python detect_pdf.py ocr-uri gs://python-docs-samples-tests/HodgeConj.pdf \
+gs://BUCKET_NAME/PREFIX/
+
+For more information, the documentation at
+https://cloud.google.com/vision/docs.
+"""
+
+import argparse
+import io
+import re
+import os
+
+from google.cloud import storage
+from google.cloud import vision
+from google.protobuf import json_format
+
+
+# [START def_detect_text]
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    # [START migration_text_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    file_name = "data_file.csv"
+    if os.path.exists(file_name):
+        apprnt_or_write = "a"
+    else:
+        apprnt_or_write = "w"
+
+    data_file = open(file_name,apprnt_or_write)
+    #print('Texts:')
+    # read only fist index from loop
+    for text in texts:
+        data_file.write('\n{}'.format(text.description))
+        print('\n{}'.format(text.description))
+        break
+
+
+
+# [START def_detect_text_uri]
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
+    """
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+# [END def_detect_text_uri]
+
+
+def run_local(args):
+    if args.command == 'text':
+        detect_text(args.path)
+
+
+def run_uri(args):
+    if args.command == 'text-uri':
+        detect_text_uri(args.uri)
+if __name__ == '__main__':
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"C:\VIVEK\GIT\google_credentials\testProj_1-2aad0580e81d.json"
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(dest='command')
+
+    detect_text_parser = subparsers.add_parser(
+        'text', help=detect_text.__doc__)
+    detect_text_parser.add_argument('path')
+
+    text_file_parser = subparsers.add_parser(
+        'text-uri', help=detect_text_uri.__doc__)
+    text_file_parser.add_argument('uri')
+
+    args = parser.parse_args()
+
+    if 'uri' in args.command:
+        run_uri(args)
+    else:
+        run_local(args)
